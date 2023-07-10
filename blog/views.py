b20251eb1aa29a49e83239f170e297e16155e83c
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-07-10 20:44:28 +0000

    Fixed edit comment

--- blog/views.py
+++ blog/views.py
@@ -50,7 +50,7 @@ class PostDetail(View):
             },
         )
 
-    def post(self, request, slug, *args, **kwargs):
+    def post(self, request, slug, commentId=None, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -58,27 +58,37 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
+        if commentId:
+            comment = get_object_or_404(
+                comments, pk=commentId, email=request.user.email)
+            comment_form = CommentForm(
+                request.POST, request.FILES, instance=comment)
+            if comment_form.is_valid():
+                comment_form.save()
+                return HttpResponseRedirect(
+                    reverse('post_detail', args=[slug]))
         else:
-            comment_form = CommentForm()
+            comment_form = CommentForm(data=request.POST)
+            commented = False
+            if comment_form.is_valid():
+                comment_form.instance.email = request.user.email
+                comment_form.instance.name = request.user.username
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.save()
+                commented = True
 
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "comment_form": comment_form,
-                "liked": liked
-            },
-        )
+            return render(
+                request,
+                "post_detail.html",
+                {
+                    "post": post,
+                    "comments": comments,
+                    "commented": commented,
+                    "comment_form": CommentForm(),
+                    "liked": liked
+                },
+            )
 
 
 class DeleteComment(View):
@@ -92,7 +102,6 @@ class DeleteComment(View):
             comment.delete()
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
 
 
 class PostLike(View):
